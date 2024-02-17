5a79ecff3a4241f6d7d5068bd0d99e4777a9f7a4
muthonicarol <muthonicaroline35@gmail.com>
2024-02-17 13:30:16 +0300

    add concatination logic

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let message = "Hello zindua!";
 let num1 = 3;
 let num2 = 2;
-let shopping = ('bread sugar')
-let solution = num1 - num2
+let shopping = ('bread' , 'sugar');
 
+let solution = num1 - num2
+let name = "Muthoni"
+
+let newMessage = `${message} my name is ${name}.`;
 console.log (solution);
+console.log (newMessage);
+console.log (shopping);
